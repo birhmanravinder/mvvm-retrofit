57d4c868c77fc0aa00fe0ca38a241e4791f64b87
Ravinder <ravinder.birhman2000@gmail.com>
2019-07-29 12:15:30 +0530

    MVVM demo

--- app/src/main/java/com/isolsgroup/rxmvvmdemo/ui/main/MainActivity.java
+++ app/src/main/java/com/isolsgroup/rxmvvmdemo/ui/main/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         ActivityComponent.component(this).inject(this);
 
         context=this;
-        viewModel.loadCategories();
+        viewModel.loadCategories();  // load category
 
         recyclerView.setLayoutManager(new GridLayoutManager(context, 3));
         adapter= new CategoryAdapter(router, viewModel.getCategories());
